0d2b6e4c4c8d13042e2346fcab61fcf8e1cbcd50
Brian H Combes <briancombes59@yahoo.com>
2018-01-21 22:26:13 -0700

    Add files via upload

---
+++ SelfEd-NodeJS-Event-Loops-Emitters-Buffers-Streams/main_Exp7.js
@@ -0,0 +1,11 @@
+// Chaining the streams - compress
+
+var fs = require("fs");
+var zlib = require('zlib');
+
+// Compress the file input.txt to input.txt.gz
+fs.createReadStream('input.txt')
+   .pipe(zlib.createGzip())
+   .pipe(fs.createWriteStream('input.txt.gz'));
+  
+console.log("File Compressed.");
